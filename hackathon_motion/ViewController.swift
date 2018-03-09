9db1331e073b00e29eed511ed848b1db214d5b29
Chi Nguyen <xx7374@gmail.com>
2018-03-08 19:24:18 -0800

    add shaking motion

--- hackathon_motion/ViewController.swift
+++ hackathon_motion/ViewController.swift
@@ -21,13 +21,17 @@ class ViewController: UIViewController {
     
     var motionManager = CMMotionManager()
     let opQueue = OperationQueue()
+    let currentCity = "San Jose"
+    let north = [["name" : "Seattle"], ["name" : "Seattle"], ["name" : "Seattle"], ["name": "Seattle"], ["name": "Seattle"]]
+    let west = [["name" : "Honolulu"], ["name" : "Honolulu"], ["name" : "Honolulu"], ["name" : "Honolulu"], ["name" : "Honolulu"]]
+    let east = [["name" : "Denver"], ["name" : "Denver"], ["name" : "Denver"], ["name" : "Denver"], ["name" : "Denver"]]
+    let south = [["name" : "San Diego"], ["name" : "San Diego"], ["name" : "San Diego"], ["name" : "San Diego"], ["name" : "San Diego"]]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if motionManager.isDeviceMotionAvailable {
+        if motionManager.isDeviceMotionActive {
             print("We can detect device motion")
-            startReadingMotionData()
         }
         else {
             print("We cannot detect device motion")
@@ -39,9 +43,55 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        if (motion == .motionShake) {
+            print("now we're shakin!")
+            var city = newCity(quad: "north")
+            print(city)
+        }
+    }
     
+    func newCity (quad: String) -> String {
+        if quad == "north" {
+            var indx = Int(arc4random_uniform(5))
+            while north[indx]["name"] == currentCity {
+                indx = Int(arc4random_uniform(5)+1)
+            }
+            if let n = north[indx]["name"] {
+                return n
+            }
+        } else if quad == "south" {
+            var indx = Int(arc4random_uniform(5))
+            while north[indx]["name"] == currentCity {
+                indx = Int(arc4random_uniform(5)+1)
+            }
+            if let n = north[indx]["name"] {
+                return n
+            }
+        }  else if quad == "east" {
+            var indx = Int(arc4random_uniform(5))
+            while east[indx]["name"] == currentCity {
+                indx = Int(arc4random_uniform(5)+1)
+            }
+            if let n = east[indx]["name"] {
+                return n
+            }
+        } else if quad == "west" {
+            var indx = Int(arc4random_uniform(5))
+            while west[indx]["name"] == currentCity {
+                indx = Int(arc4random_uniform(5)+1)
+            }
+            if let n = west[indx]["name"] {
+                return n
+            }
+        }
+        
+        return "";
+    }
+   
     func startReadingMotionData() {
         // set read speed
+        
         motionManager.deviceMotionUpdateInterval = 1
         // start reading
         motionManager.startDeviceMotionUpdates(to: opQueue) {
